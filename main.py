69aa6310b7dfae07088b70ce9f501988643388b8
unholydeath785 <ivansonofcoul@gmail.com>
2018-01-29 13:50:41 -0800

    added a sprite sheet with sprite positions, as well as updating screen size

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import pygame
+from InvaderSheet import InvaderSheet
+from InvaderSheet import Sprites
+
 pygame.init()
 
 BLACK = (0,0,0)
-WHITE = (255,255,255)
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
 
-size = (700, 500)
+size = (720, 640)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("WebDev Python and Git Practice")
+sheet = InvaderSheet()
 
 running = True
 clock = pygame.time.Clock()
+
+def render(image, pos):
+	width = image.get_width()
+	height = image.get_height()
+	size = [int(64 * (width / height)), int(64 * (height / width))]
+	scaled_image = pygame.transform.scale(image, (size[0], size[1]))
+	screen.blit(scaled_image, pos)
 
 while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
-	screen.fill(WHITE)
-
-	pygame.draw.rect(screen, RED, [55, 200, 100, 70],0)
-	pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
-	pygame.draw.ellipse(screen, BLUE, [20,20,250,100], 2)
+	screen.fill(BLACK)
 
 	pygame.display.flip()
 	clock.tick(60)
